acd91a55dc76301555cd4af63f1d9359989702e8
recepdemirr <recep.dmr2334@gmail.com>
2024-08-11 18:59:27 +0300

    Controller da CRUD Oluşturuldu.

---
+++ KARE_Tracker_API/KARE_Tracker_API/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using KARE_Tracker_API.Bussiness.Abstract;
+using KARE_Tracker_API.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KARE_Tracker_API.Controllers
+{
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _Service;
+
+        public ProductController(IProductService service)
+        {
+            _Service = service;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_Service.GetAll());
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Products products)
+        {
+            return Ok(_Service.Add(products));
+        }
+        [HttpPost("Delete")]
+        public IActionResult Delete(int id)
+        {
+            _Service.Delete(id);
+            return Ok();
+        }
+        [HttpPost("Update")]
+        public IActionResult Update(Products products)
+        {
+            return Ok(_Service.Update(products));
+        }
+    }
+}
---
+++ KARE_Tracker_API/KARE_Tracker_API/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using KARE_Tracker_API.Bussiness.Abstract;
+using KARE_Tracker_API.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KARE_Tracker_API.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusService _Service;
+
+        public StatusController(IStatusService  Service)
+        {
+            _Service = Service;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_Service.GetAll());
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Status statuses)
+        {
+            return Ok(_Service.Add(statuses));
+        }
+        [HttpPost("Delete")]
+        public IActionResult Delete(int id)
+        {
+            _Service.Delete(id);
+            return Ok();
+        }
+        [HttpPost("Update")]
+        public IActionResult Update(Status statuses)
+        {
+            return Ok(_Service.Update(statuses));
+        }
+    }
+}
