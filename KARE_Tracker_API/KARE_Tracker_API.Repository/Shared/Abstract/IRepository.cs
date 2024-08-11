d2ca6b2050e141d182e9e92b07a8659c4708fc8b
recepdemirr <recep.dmr2334@gmail.com>
2024-08-11 17:29:42 +0300

    Repository Katmanı Oluşturuldu.

---
+++ KARE_Tracker_API/KARE_Tracker_API.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using KARE_Tracker_API.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARE_Tracker_API.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T,bool>> predicate);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(int id);
+        void Save();
+
+    }
+}
---
+++ KARE_Tracker_API/KARE_Tracker_API.Repository/Shared/Concrete/Repository.cs
@@ -0,0 +1,69 @@
+﻿using KARE_Tracker_API.Data;
+using KARE_Tracker_API.Models.Models;
+using KARE_Tracker_API.Repository.Shared.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARE_Tracker_API.Repository.Shared.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbset = _context.Set<T>();
+        }
+
+        private DbSet<T> _dbset;
+       
+
+        public T Add(T entity)
+        {
+            _dbset.Add(entity);
+            Save();
+            return entity;
+        }
+
+        public void Delete(int id)
+        {
+           T entity = _dbset.Find(id);
+            entity.IsDeleted = false;
+            _dbset.Update(entity);
+            Save();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return GetAll().Where(predicate);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbset.Where(x => !x.IsDeleted);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return _dbset.FirstOrDefault(predicate);
+        }
+
+        public void Save()
+        {
+           _context.SaveChanges();
+        }
+
+        public T Update(T entity)
+        {
+            _dbset.Update(entity);
+            Save();
+            return entity;
+        }
+    }
+}
