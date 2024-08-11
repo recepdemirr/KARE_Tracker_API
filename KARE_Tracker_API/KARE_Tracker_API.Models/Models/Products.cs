bb5f0f019d4a73ea9c93dfddafd5e5c351646da7
recepdemirr <recep.dmr2334@gmail.com>
2024-08-11 16:47:24 +0300

    modeller oluşturuldu.

---
+++ KARE_Tracker_API/KARE_Tracker_API.Models/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARE_Tracker_API.Models.Models
+{
+    public class Products : BaseModel
+    {
+        public DateTime DateCreated { get; set; }
+        public int Day { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+
+
+    }
+}
---
+++ KARE_Tracker_API/KARE_Tracker_API.Models/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARE_Tracker_API.Models.Models
+{
+    public class Status: BaseModel
+    {
+       public ICollection<Products> Products { get; set; }
+    }
+}
